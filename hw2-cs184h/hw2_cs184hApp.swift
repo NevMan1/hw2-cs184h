2ad9a5a15ea19600e6cd362d7d0b7a1f91b0b7e3
Nevin Manimaran <nevin@ucsb.edu>
2024-10-22 11:06:36 -0700

    Initial Commit

---
+++ hw2-cs184h/hw2_cs184hApp.swift
@@ -0,0 +1,17 @@
+//
+//  hw2_cs184hApp.swift
+//  hw2-cs184h
+//
+//  Created by Nevin Manimaran on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct hw2_cs184hApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hw2-cs184hTests/hw2_cs184hTests.swift
@@ -0,0 +1,17 @@
+//
+//  hw2_cs184hTests.swift
+//  hw2-cs184hTests
+//
+//  Created by Nevin Manimaran on 10/22/24.
+//
+
+import Testing
+@testable import hw2_cs184h
+
+struct hw2_cs184hTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
